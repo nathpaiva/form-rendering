31db7db3c646963609ce5dd30a49892abe0a2615
Nath Paiva <hello@nathpaiva.com.br>
2023-08-02 15:39:32 -0700

    feat: creates tests for not required field

--- src/helpers/__tests__/prepareRenderInput.test.ts
+++ src/helpers/__tests__/prepareRenderInput.test.ts
@@ -3,98 +3,147 @@ import { fireEvent, screen } from '@testing-library/dom'
 import { RequestFields, UserFields } from '../../types'
 import { buildForm } from '../prepareRenderInput'
 
-describe('prepareRenderInput', () => {
-  it('should create section with checkbox input', () => {
-    const fieldData = {
-      reference: null,
-      name: 'What will be the service?',
-      label: 'What will be the service?',
-      placeholder: 'What will be the service?',
-      mask: 'service type',
-      type: 'enumerable',
-      required: true,
-      allow_multiple_value: true,
-      allow_custom_value: false,
-      values: {
-        Dye: 'Dye',
-        Haircut: 'Haircut',
-      },
-      _embedded: {
-        nested_fields: [],
-      },
-    } satisfies RequestFields
+const expectsCheckbox = (form: string, props: RequestFields) => {
+  document.body.innerHTML = form
+  const label = screen.getByText(props.label)
 
-    const form = buildForm(fieldData)
+  expect(label.tagName).toBe('LABEL')
+  expect(label).toBeTruthy()
 
-    document.body.innerHTML = form
-    const label = screen.getByText(fieldData.label)
-
-    expect(label.tagName).toBe('LABEL')
-    expect(label).toBeTruthy()
-
+  if (props.required) {
     const errorMessage = screen.getByText('Choose at least one option.')
     expect(errorMessage).toBeTruthy()
+  }
 
-    Object.keys(fieldData.values).forEach((key) => {
-      const keyValue = fieldData.values[key as keyof typeof fieldData.values]
-      const input = screen.getByLabelText(keyValue)
+  Object.keys(props.values).forEach((key) => {
+    const keyValue = props.values[key as keyof typeof props.values]
+    const input = screen.getByLabelText(keyValue)
 
-      expect(input.tagName).toBe('INPUT')
-      expect(input).toHaveProperty('type', 'checkbox')
+    expect(input.tagName).toBe('INPUT')
+    expect(input).toHaveProperty('type', 'checkbox')
+    if (props.required) {
       expect(input.classList.contains('required')).toBeTruthy()
-      expect(input).toHaveProperty('value', keyValue)
-      expect(input).not.toBeChecked()
+    } else {
+      expect(input.classList.contains('required')).toBeFalsy()
+    }
+    expect(input).toHaveProperty('value', keyValue)
+    expect(input).not.toBeChecked()
 
-      input.click()
+    input.click()
 
-      expect(input).toBeChecked()
+    expect(input).toBeChecked()
+  })
+}
+
+const expectsSelect = (form: string, props: RequestFields) => {
+  document.body.innerHTML = form
+  const label = screen.getByText(props.label)
+
+  expect(label.tagName).toBe('LABEL')
+  expect(label).toBeTruthy()
+
+  const select = screen.getByTestId(props.name)
+
+  expect(select.tagName).toBe('SELECT')
+  expect(select).toBeTruthy()
+  expect(select).toHaveValue('')
+  if (props.required) {
+    expect(select.classList.contains('required')).toBeTruthy()
+  } else {
+    expect(select.classList.contains('required')).toBeFalsy()
+  }
+
+  Object.keys(props.values).forEach((key, index) => {
+    const keyValue = props.values[key as keyof typeof props.values]
+    const option = screen.getByText(keyValue)
+
+    expect(option.tagName).toBe('OPTION')
+
+    fireEvent.change(select, { target: { value: index.toString() } })
+
+    expect(select).toHaveValue(index.toString())
+  })
+}
+
+describe('prepareRenderInput', () => {
+  describe('checkbox', () => {
+    const checkboxFieldData = (isRequired = true): RequestFields => {
+      const data = {
+        reference: null,
+        name: 'What will be the service?',
+        label: 'What will be the service?',
+        placeholder: 'What will be the service?',
+        mask: 'service type',
+        type: 'enumerable',
+        required: isRequired,
+        allow_multiple_value: true,
+        allow_custom_value: false,
+        values: {
+          Dye: 'Dye',
+          Haircut: 'Haircut',
+        },
+        _embedded: {
+          nested_fields: [],
+        },
+      } satisfies RequestFields
+
+      return data
+    }
+
+    it('should create required section with checkbox input', () => {
+      const props = checkboxFieldData()
+      const form = buildForm(props)
+
+      expectsCheckbox(form, props)
+    })
+
+    it('should create not required section with checkbox input', () => {
+      const props = checkboxFieldData(false)
+      const form = buildForm(props)
+
+      expectsCheckbox(form, props)
     })
   })
 
-  it('should create section with select', () => {
-    const fieldData = {
-      reference: null,
-      name: 'How many people will the service be for?',
-      label: 'How many people will the service be for?',
-      placeholder: 'How many people will the service be for?',
-      mask: 'add the number of people',
-      type: 'enumerable',
-      required: false,
-      allow_multiple_value: false,
-      allow_custom_value: false,
-      values: {
-        '1': '1',
-        '2': '2',
-        'More than 3': 'More than 3',
-      },
-      _embedded: {
-        nested_fields: [],
-      },
-    } satisfies RequestFields
+  describe('select', () => {
+    const selectFieldData = (isRequired = true): RequestFields => {
+      const data = {
+        reference: null,
+        name: 'How many people will the service be for?',
+        label: 'How many people will the service be for?',
+        placeholder: 'How many people will the service be for?',
+        mask: 'add the number of people',
+        type: 'enumerable',
+        required: isRequired,
+        allow_multiple_value: false,
+        allow_custom_value: false,
+        values: {
+          '1': '1',
+          '2': '2',
+          'More than 3': 'More than 3',
+        },
+        _embedded: {
+          nested_fields: [],
+        },
+      } satisfies RequestFields
 
-    const form = buildForm(fieldData)
+      return data
+    }
 
-    document.body.innerHTML = form
-    const label = screen.getByText(fieldData.label)
+    it('should create required section with select', () => {
+      const props = selectFieldData()
 
-    expect(label.tagName).toBe('LABEL')
-    expect(label).toBeTruthy()
+      const form = buildForm(props)
 
-    const select = screen.getByTestId(fieldData.name)
+      expectsSelect(form, props)
+    })
 
-    expect(select.tagName).toBe('SELECT')
-    expect(select).toBeTruthy()
-    expect(select).toHaveValue('')
+    it('should create not required section with select', () => {
+      const props = selectFieldData(false)
 
-    Object.keys(fieldData.values).forEach((key, index) => {
-      const keyValue = fieldData.values[key as keyof typeof fieldData.values]
-      const option = screen.getByText(keyValue)
+      const form = buildForm(props)
 
-      expect(option.tagName).toBe('OPTION')
-
-      fireEvent.change(select, { target: { value: index.toString() } })
-
-      expect(select).toHaveValue(index.toString())
+      expectsSelect(form, props)
     })
   })
 
